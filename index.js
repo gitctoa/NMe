a94a11c171496a7262257f3d83de19f4de0be6b2
gitctoa <joszy2682@gmail.com>
2025-07-22 16:57:38 +0530

    finallll

--- index.js
+++ index.js
@@ -34,25 +34,27 @@ gsap.utils.toArray(".bento-card").forEach((card) => {
 const waitlistForm = document.querySelector(".waitlist-ctn");
 waitlistForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const email = document.querySelector(".email-ctn").value;
+  const emailCtn = document.querySelector(".email-ctn");
+  const email = emailCtn.value;
   const btn = document.querySelector(".join-btn");
+  payload=`${window.location.origin}|${email}`
   btnOldHTML=btn.innerHTML
   async function appendEmail() {
+    emailCtn.disabled=true;
     btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="spinner " viewBox="0 0 16 16">
                       <path d="M8.932.727c-.243-.97-1.62-.97-1.864 0l-.071.286a.96.96 0 0 1-1.622.434l-.205-.211c-.695-.719-1.888-.03-1.613.931l.08.284a.96.96 0 0 1-1.186 1.187l-.284-.081c-.96-.275-1.65.918-.931 1.613l.211.205a.96.96 0 0 1-.434 1.622l-.286.071c-.97.243-.97 1.62 0 1.864l.286.071a.96.96 0 0 1 .434 1.622l-.211.205c-.719.695-.03 1.888.931 1.613l.284-.08a.96.96 0 0 1 1.187 1.187l-.081.283c-.275.96.918 1.65 1.613.931l.205-.211a.96.96 0 0 1 1.622.434l.071.286c.243.97 1.62.97 1.864 0l.071-.286a.96.96 0 0 1 1.622-.434l.205.211c.695.719 1.888.03 1.613-.931l-.08-.284a.96.96 0 0 1 1.187-1.187l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205a.96.96 0 0 1 .434-1.622l.286-.071c.97-.243.97-1.62 0-1.864l-.286-.071a.96.96 0 0 1-.434-1.622l.211-.205c.719-.695.03-1.888-.931-1.613l-.284.08a.96.96 0 0 1-1.187-1.186l.081-.284c.275-.96-.918-1.65-1.613-.931l-.205.211a.96.96 0 0 1-1.622-.434zM8 12.997a4.998 4.998 0 1 1 0-9.995 4.998 4.998 0 0 1 0 9.996z"/>
                     </svg>`;
     try {
-      const result = fetch(
-        "https://script.google.com/macros/s/AKfycbxExS06Y6BCRzvDny81SOy4aDRrhubooYQR57uueP2hXONTSj4GTA3IZy2HSTgM61vz/exec",
+      const result = await fetch(
+        "https://script.google.com/macros/s/AKfycbxV7JDCznL5Vmh7XKEsk6DvGjkMN9bGGvP9s76gdj50OhCB6MkiZUYpeWsB3MmqWckB/exec",
         {
           method: "POST",
           headers: {
             "Content-Type": "text/plain; charset=UTF-8",
           },
-          body: email,
+          body: payload,
         }
       );
-      console.log(await result)
       const text = await result.text();
       console.log("done")
       if (text === "EXISTS") {
