576992e9a553d6b3d652f9b3b3b62fc9e0f2b62c
gitctoa <joszy2682@gmail.com>
2025-07-22 14:27:34 +0530

    asas

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ waitlistForm.addEventListener("submit", (e) => {
   const data = { timeZone: timeZone, email: email };
 
   fetch(
-    "https://script.google.com/macros/s/AKfycbw-8Y6gHUo4iimQRblz52AxtM4ge93nbUJotlw08_9mYbxuRsjdI60N5KWqtL6yVPVL/exec",
+    "https://script.google.com/macros/s/AKfycbwlxBwVbWM1ozBK5v7L85pMQKZKVjaE6HCUQ39FvJtBHqln7czdxb8mmnbvKWOAYDeN/exec",
     {
       method: "POST",
       mode: "cors",
